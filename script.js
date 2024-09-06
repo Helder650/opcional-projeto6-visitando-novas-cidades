232aa5091be261b8192e25df4ddf48f4d9619f16
Helder650 <helderblm09@gmail.com>
2024-09-05 21:44:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let tourist = prompt("Qual seu nome turista?")
+let city = prompt("Você visitou alguma cidade? (s/n)")
+let citylist = []
+let numbercity = 0
+
+while(city == 's') {
+  citylist[numbercity] = prompt("Qual cidade voce visitou?")
+  numbercity += 1
+
+  city = prompt("Você visitou alguma outra cidade? (s/n)")
+
+}
+alert(`Turista: ${tourist}
+Quantidade de cidades visitadas: ${numbercity}
+Cidades visitadas: ${citylist}`)
