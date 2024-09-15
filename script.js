db9f35a81439f095837268853efc202f950ec2b8
Helder650 <helderblm09@gmail.com>
2024-09-15 00:48:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 let tourist = prompt("Qual seu nome turista?")
 let city = prompt("Você visitou alguma cidade? (s/n)")
-let citylist = []
+let citylist = ""
 let numbercity = 0
 
 while(city == 's') {
-  citylist[numbercity] = prompt("Qual cidade voce visitou?")
-  numbercity += 1
-
-  city = prompt("Você visitou alguma outra cidade? (s/n)")
-
+  if (numbercity > 0) {
+    citylist += ","
+  }
+citylist += prompt("Qual cidade voce visitou?")
+numbercity += 1
+city = prompt("Você visitou alguma outra cidade? (s/n)")
 }
+
 alert(`Turista: ${tourist}
 Quantidade de cidades visitadas: ${numbercity}
 Cidades visitadas: ${citylist}`)
